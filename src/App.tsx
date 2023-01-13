d6cb2114a591e04ab0a26d65d09f96468c9f6045
Rafael Assad <rafael.assad1010@gmail.com>
2023-01-13 13:09:19 -0300

    Hotfix/ Fixed the portifolio menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import { useLocation } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import PortifolioMenu from "./components/PortifolioMenu"
+
 import Routes from "./routes";
 
 import { AppContainer, MainContainer } from "./styles/appStyles";
 import { GlobalStyle } from "./styles/global";
 
 const App = () => {
-  const location = useLocation()
-
-  const currentPage = location.pathname
-
   return (
     <AppContainer>
       <GlobalStyle/>
 
       <Header/>
 
-      {currentPage === "/portifolio" && <PortifolioMenu/>}
+
 
     <MainContainer>
       <Routes/>
